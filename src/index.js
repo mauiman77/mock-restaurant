1665884a035c65a14cd50c94591fd76a9a5cbaac
mauiman77 <boerbasher@protonmail.com>
2023-04-16 15:40:26 +0100

    Add title, buttons

--- src/index.js
+++ src/index.js
@@ -5,15 +5,39 @@ const mainSection = document.getElementById('content');
 function headerSection () {
   const siteHeader = document.createElement('div');
   siteHeader.classList.add('header');
-  siteHeader.textContent = 'Test!Test!';
+  siteHeader.appendChild(headerSectionTitle());
+  siteHeader.appendChild(headerSectionButtons())
 
   return siteHeader;
+}
+
+function headerSectionTitle () {
+  const headerTitle = document.createElement('div');
+  headerTitle.classList.add('title')
+  headerTitle.textContent = 'Aye-ce Creams';
+
+  return headerTitle;
+}
+
+function headerSectionButtons () {
+  const headerButtons = document.createElement('div');
+  headerButtons.classList.add('buttons');
+  const btnTexts = ['Home', 'Menu', 'Contact'];
+  for (let i = 0; i < 3; i++) {
+    const btn = document.createElement('div');
+    btn.classList.add('btn');
+    const textToAdd = btnTexts[i];
+    btn.textContent = textToAdd;
+    headerButtons.appendChild(btn)
+  }
+
+  return headerButtons;
 }
 
 function bodySection () {
   const bodyContent = document.createElement('div');
   bodyContent.classList.add('main');
-  bodyContent.textContent = 'Hello!!'
+  bodyContent.textContent = 'HELLLLO!!'
 
   return bodyContent;
 }
