97f21eba930a87fc5cb7fddf5b488cfd37aea266
mauiman77 <boerbasher@protonmail.com>
2023-04-16 13:50:17 +0100

    Transfer CSS to WebPack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-const mainBit = document.getElementById('content')
+import './style.css';
 
-function component() {
-    const siteHeader = document.createElement('div');
-    siteHeader.className = 'header'
- 
-    siteHeader.textContent = 'Test!';
- 
-    return siteHeader;
+const mainSection = document.getElementById('content');
+
+function headerSection () {
+  const siteHeader = document.createElement('div');
+  siteHeader.classList.add('header');
+  siteHeader.textContent = 'Test!Test!';
+
+  return siteHeader;
 }
 
-mainBit.appendChild(component());
+function bodySection () {
+  const bodyContent = document.createElement('div');
+  bodyContent.classList.add('main');
+  bodyContent.textContent = 'Hello!!'
+
+  return bodyContent;
+}
+
+function footerSection () {
+  const footerContent = document.createElement('div');
+  footerContent.classList.add('footer');
+  footerContent.textContent = 'Footer footer!';
+
+  return footerContent;
+}
+
+mainSection.appendChild(headerSection());
+mainSection.appendChild(bodySection());
+mainSection.appendChild(footerSection());
